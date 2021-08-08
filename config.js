c482f8fbb98938b928dddfd055d77fc267feb694
seema3344 <74967300+seema3344@users.noreply.github.com>
2021-08-08 14:27:24 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC5sMgJLNJIGW5vasOsUCHXy2AxECUXv8A",
+    authDomain: "car-app-9fb06.firebaseapp.com",
+    databaseURL: "https://car-app-9fb06-default-rtdb.firebaseio.com",
+    projectId: "car-app-9fb06",
+    storageBucket: "car-app-9fb06.appspot.com",
+    messagingSenderId: "89799045316",
+    appId: "1:89799045316:web:6388062bfc9b6f8b45d991"
+  };
